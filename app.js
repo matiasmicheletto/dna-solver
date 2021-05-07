76b3ff183be8bf0ca0b23e6a18c45580cac6031b
matias <matiasmicheletto@gmail.com>
2021-05-07 12:18:45 -0300

    Connection back and front

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 const Server = require('./models/server');
 const server = new Server();
+
 server.listen();
--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT; // Loaded from .env file
+    this.port = process.env.PORT;
     this.paths = {
       auth: "/api/auth",
       homepage: "/api/homepage",
@@ -16,13 +16,25 @@ class Server {
   }
 
   middlewares() {
-    this.app.use(cors()); // Enable CORS
+    this.app.use(cors());
+    this.app.use(express.json());
+
+    // Pick up React index.html file
+    this.app.use(
+      express.static(path.join(__dirname, "../client/build"))
+    );
   }
 
   // Bind controllers to routes
   routes() {
     this.app.use(this.paths.auth, require("../routes/auth"));
     this.app.use(this.paths.homepage, require("../routes/homepage"));
+    // Catch all requests that don't match any route
+    this.app.get("*", (req, res) => {
+      res.sendFile(
+        path.join(__dirname, "../client/build/index.html")
+      );
+    });
   }
 
   listen() {
