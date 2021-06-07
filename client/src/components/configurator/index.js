85f89ed86d818899b52126c36f6ad85f5f86130c
matias <matiasmicheletto@gmail.com>
2021-06-07 19:00:02 -0300

    Configurator component

---
+++ client/src/components/configurator/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useContext } from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
+import { FaPlus } from 'react-icons/fa';
+import FitnessSelect from '../fitnessselect';
+import { fitness as fitness_type } from '../../manager';
+import FitnessItem from '../fitnessitem';
+import classes from './styles.module.css';
+import { OMContext } from '../../ManagerContext';
+
+const Configurator = props => {
+
+    const om = useContext(OMContext);
+
+    const [f_type, setFType] = useState(fitness_type.TSP);
+    const [fitness_list, setFitnessList] = useState([]);
+
+    const add_fitness = () => {
+        om.add_fitness(f_type);
+        setFitnessList([...om.fitness]);
+    };
+
+    const fitnessSelection = e => { // Fitness select callback
+        setFType(e.target.value);
+    };
+
+    return (
+        <div>
+            <Row className={classes.FitnessSelectContainer}>
+                <FitnessSelect onChange={fitnessSelection} />
+            </Row>
+            {fitness_list.map( (f, ind) => <FitnessItem key={ind} fitness={f} /> )}
+            <Row>
+                <Col sm={{span: 1, offset:11}}>
+                    <Button 
+                        variant="success"
+                        className={[classes.BtnRnd, classes.AddFitnessBtn]} 
+                        onClick={add_fitness}
+                        title="Add Fitness Function">
+                        <FaPlus />
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default Configurator;
--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,47 +1,12 @@
-import React, { useState, useContext } from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
-import { FaPlus } from 'react-icons/fa';
-import FitnessSelect from './fitness_select';
-import { fitness as fitness_type } from '../../manager';
-import FitnessItem from '../fitnessitem';
-import classes from './styles.module.css';
-import { OMContext } from '../../ManagerContext';
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import Configurator from '../configurator';
 
-const Dashboard = props => {
 
-    const om = useContext(OMContext);
-
-    const [f_type, setFType] = useState(fitness_type.TSP);
-    const [fitness_list, setFitnessList] = useState([]);
-
-    const add_fitness = () => {
-        om.add_fitness(f_type);
-        setFitnessList([...om.fitness]);
-    };
-
-    const fitnessSelection = e => { // Fitness select callback
-        setFType(e.target.value);
-    };
-
-    return (
-        <div>
-            <Row className={classes.FitnessSelectContainer}>
-                <FitnessSelect onChange={fitnessSelection} />
-            </Row>
-            {fitness_list.map( (f, ind) => <FitnessItem key={ind} fitness={f} /> )}
-            <Row>
-                <Col sm={{span: 1, offset:11}}>
-                    <Button 
-                        variant="success"
-                        className={[classes.BtnRnd, classes.AddFitnessBtn]} 
-                        onClick={add_fitness}
-                        title="Add Fitness Function">
-                        <FaPlus />
-                    </Button>
-                </Col>
-            </Row>
-        </div>
-    );
-}
+const Dashboard = props => (
+    <Row>
+        <Configurator />
+    </Row>
+);
 
 export default Dashboard;
